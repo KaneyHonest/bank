8053751b90a3c0c6463fbfb99e92a6a781153930
kaney <yamadataro0407@gmail.com>
2023-04-19 23:19:12 +0900

    first commit

---
+++ bank/src/main/java/logic/RegisterUserLogic.java
@@ -0,0 +1,25 @@
+package logic;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import database.DBManager;
+import model.User;
+
+public class RegisterUserLogic {
+	public void execute(User user) {
+		Connection con = null;
+		PreparedStatement ps = null;
+		try {
+			con = DBManager.getConnection();
+			ps = con.prepareStatement("insert into user (name, password, accountNumber) values (?, ?, ?)");
+			ps.setString(1, user.getName());
+			ps.setString(2, user.getPassword());
+			ps.setString(3, user.getAccountNumber());
+			ps.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} 
+	}
+}
